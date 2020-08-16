d3a48f6bee3c2d3497b9d44279e42820386c6715
xiewenhua <vievenvua@gmail.com>
2020-08-16 16:20:03 +0800

    add stores

---
+++ SaleAppMVCSolution/SaleAppMVC/Controllers/storesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SaleAppMVC.Models;
+
+namespace SaleAppMVC.Controllers
+{
+    public class storesController : Controller
+    {
+        private HomeworksDBEntities db = new HomeworksDBEntities();
+
+        // GET: stores
+        public ActionResult Index()
+        {
+            return View(db.stores.ToList());
+        }
+
+        // GET: stores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            store store = db.stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // GET: stores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: stores/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性。有关
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "stor_id,stor_address")] store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.stores.Add(store);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(store);
+        }
+
+        // GET: stores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            store store = db.stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // POST: stores/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性。有关
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "stor_id,stor_address")] store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(store).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(store);
+        }
+
+        // GET: stores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            store store = db.stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // POST: stores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            store store = db.stores.Find(id);
+            db.stores.Remove(store);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
