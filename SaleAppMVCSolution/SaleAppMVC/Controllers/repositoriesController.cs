d15ab7c4b036c3c4be234144fd9c6f64e17a1a34
xiewenhua <vievenvua@gmail.com>
2020-08-16 16:25:56 +0800

    add repositories

---
+++ SaleAppMVCSolution/SaleAppMVC/Controllers/repositoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SaleAppMVC.Models;
+
+namespace SaleAppMVC.Controllers
+{
+    public class repositoriesController : Controller
+    {
+        private HomeworksDBEntities db = new HomeworksDBEntities();
+
+        // GET: repositories
+        public ActionResult Index()
+        {
+            return View(db.repositories.ToList());
+        }
+
+        // GET: repositories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            repository repository = db.repositories.Find(id);
+            if (repository == null)
+            {
+                return HttpNotFound();
+            }
+            return View(repository);
+        }
+
+        // GET: repositories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: repositories/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性。有关
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "repo_id,repo_address")] repository repository)
+        {
+            if (ModelState.IsValid)
+            {
+                db.repositories.Add(repository);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(repository);
+        }
+
+        // GET: repositories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            repository repository = db.repositories.Find(id);
+            if (repository == null)
+            {
+                return HttpNotFound();
+            }
+            return View(repository);
+        }
+
+        // POST: repositories/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性。有关
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "repo_id,repo_address")] repository repository)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(repository).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(repository);
+        }
+
+        // GET: repositories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            repository repository = db.repositories.Find(id);
+            if (repository == null)
+            {
+                return HttpNotFound();
+            }
+            return View(repository);
+        }
+
+        // POST: repositories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            repository repository = db.repositories.Find(id);
+            db.repositories.Remove(repository);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
