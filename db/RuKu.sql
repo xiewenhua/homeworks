6d83bccccf2ec3018dd77c02cb57d789145df06b
xiewenhua <vievenvua@gmail.com>
2020-08-19 06:00:47 +0800

    删除手动输入自增id

--- db/RuKu.sql
+++ db/RuKu.sql
@@ -1,47 +1,35 @@
 -- 创建入库商品类型。因为入库的可能是多种商品, SQL Server 支持Table类型（MySQL是不支持的）
 CREATE TYPE goodlisttype AS TABLE(
-    good_id int NOT NULL,
-    purchase_price float NOT NULL,
-    purchase_quantity int NOT NULL
+    good_id int NOT NULL,   -- 商品编号
+    purchase_price float NOT NULL,  -- 进货价格
+    purchase_quantity int NOT NULL  --数量
 )
 GO
 
 -- 创建入库单的存储过程
 CREATE PROC ruku(
-    @batch_id int,
-    @repo_id int,
-    @batch_date datetime,
-    @vend_id int,
-    @goodlist goodlisttype  READONLY
+    @repo_id int,   -- 仓库编号
+    @vend_id int,   -- 供应商编号 
+    @goodlist goodlisttype READONLY -- 进货详情
 )
 AS
-BEGIN
-    BEGIN TRANSACTION
-    SET IDENTITY_INSERT batch ON
-    INSERT INTO
-    batch(batch_id, repo_id, batch_date, vend_id)
-    VALUES
-        (@batch_id, @repo_id, @batch_date, @vend_id)
-    SET IDENTITY_INSERT batch OFF
-    INSERT INTO
-    batch_goods
-        (
-        batch_id,
-        good_id,
-        purchase_price,
-        purchase_quantity,
-        surplus
-        )
-    SELECT
-        @batch_id,
-        good_id,
-        purchase_price,
-        purchase_quantity,
-        good_id
-    FROM
-        @goodlist
-    COMMIT TRANSACTION
-END
+DECLARE @batch_id int   -- 进货批次号
+BEGIN TRANSACTION
+
+-- 生成批次号
+INSERT INTO batch
+    (repo_id, batch_date, vend_id)
+VALUES
+    (@repo_id, GETDATE(), @vend_id);
+SET @batch_id = SCOPE_IDENTITY();   -- 获取自增ID
+
+-- 插入进货清单
+INSERT INTO batch_goods
+    (batch_id, good_id, purchase_price, purchase_quantity, surplus)
+SELECT @batch_id, good_id, purchase_price, purchase_quantity, purchase_quantity
+FROM @goodlist
+
+COMMIT TRANSACTION
 GO
 
 -- 使用存储过程分两步
@@ -54,5 +42,5 @@ UNION ALL
 UNION ALL
     SELECT 10003, 788, 1000
 -- 2、执行存储过程
-EXEC ruku @batch_id =10001,@repo_id= 10001,@batch_date='2020-07-03',@vend_id=1001,@goodlist=@@goodlist
+EXEC ruku @repo_id= 10001,@vend_id=1001,@goodlist=@@goodlist
 GO
